6ce26161f64cf10ae608b1223289aaca05de33a1
Nishant Bhagat <64642374+Nishantbhagat57@users.noreply.github.com>
2023-05-11 21:30:51 +0530

    Create verify.sh

---
+++ verify.sh
@@ -0,0 +1,1 @@
+DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="4/0AbUR2VPYcB9CMfdTTylWTQbQKfMmtqgtqaS2Z6WMgsRS3UxoljWf3wx9Eb4jOd6JuocNVA" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname)
