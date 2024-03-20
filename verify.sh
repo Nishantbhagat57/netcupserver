65f734614c1b04762ef4892f9161166a6b2f1029
Nishant Bhagat <nishantbhagat57@gmail.com>
2024-03-20 18:22:27 +0530

    Update verify.sh

--- verify.sh
+++ verify.sh
@@ -1,1 +1,1 @@
-DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="4/0AeaYSHCcCvxdxvtji7xzZc2c07u0DHkINdQJk0TUtHXaYXpdBIR4RAMqT0GrT3goSayOiA" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname)
+DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="4/0AeaYSHB7KXGhcJL-ms6CmnMlkBZHrzYDCZ2kJA1Phma93i9NaOjydFOc4e4w6E1xvT7mjw" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname)
